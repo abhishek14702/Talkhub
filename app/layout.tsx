b8df0be6d60e9da4b9ad4354bfb21ae1dc1360ee
abhishek14702 <72705297+abhishek14702@users.noreply.github.com>
2023-09-30 02:22:07 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,13 +8,13 @@ import ActiveStatus from './components/ActiveStatus';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Nexus - Your Ultimate Chat Experience',
+  title: 'TalkHub',
   description:
-    'Nexus is a revolutionary chat application that transcends traditional messaging. Immerse yourself in rich, dynamic conversations, collaborate effortlessly, and forge meaningful connections with friends and colleagues. Join Nexus today and experience a new era of communication.',
+    'TalkHub is a revolutionary chat application that transcends traditional messaging. Immerse yourself in rich, dynamic conversations, collaborate effortlessly, and forge meaningful connections with friends and colleagues. Join TalkHub today and experience a new era of communication.',
   keywords: [],
   twitter: {
     card: 'summary_large_image',
-    creator: '@yodkwtf',
+    creator: 'https://abhi-one.vercel.app/#home',
   },
 };
 
